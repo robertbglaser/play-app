4bbd74458147bc3b9904a9d648cb91070f67a097
Bob Glaser <bobglaser@Bobs-MacBook-Air.local>
2018-10-06 14:46:00 -0700

    flex tutorial

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <div>I am a react app</div>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container-1">
+        <div className="box-1">
+          <h3>Box One</h3>
+            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit .  </p>
+        </div>
+        <div className="box-2">
+            <h3>Box Two</h3>
+              <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit . </p>
+            </div>
+        <div className="box-3">
+            <h3>Box Three</h3>
+            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit .</p>
+        </div>
+        <div className="container-2">
+            <div className="container-2-box">
+              <h3>Box Four</h3>
+                <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit .  </p>
+            </div>
+            <div className="container-2-box">
+                <h3>Box Five</h3>
+                  <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit . </p>
+                </div>
+            <div className="container-2-box">
+                <h3>Box Six</h3>
+                <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit .</p>
+            </div>
+          </div>
       </div>
+      
+      
     );
   }
 }
