fa19f2a727c7282c00d8c77b3aa9697ed8604cc9
Bob Glaser <bobglaser@Bobs-MacBook-Air.local>
2018-10-05 18:02:37 -0700

    added a test div

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <div>I am a react app</div>
           <a
             className="App-link"
             href="https://reactjs.org"
